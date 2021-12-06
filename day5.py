8fda20c6d8931ea4cc12926bfe61701220c83921
Will McKinnon <contact@willmckinnon.com>
2021-12-06 00:49:46 -0500

    improved day5 again

--- day5.py
+++ day5.py
@@ -4,20 +4,14 @@ import copy
 f = open('input/day5.txt', 'r')
 
 coordinatesList = []
-maxX = 0
-maxY = 0
 
 buffer = f.readline()[:-1]
 while buffer:
     coordinates = [int(i) for i in re.split(r',| -> ', buffer)]
-    maxX = max(maxX, coordinates[0], coordinates[2])
-    maxY = max(maxY, coordinates[1], coordinates[3])
     coordinatesList.append(coordinates)
     buffer = f.readline()[:-1]
 
-board = [[0]*(maxX + 1) for _ in range(maxY + 1)]
-
-def listFromRange(valOne, valTwo) -> list:
+def listFromRange(valOne: int, valTwo: int) -> list:
     if valOne > valTwo:
         return list(range(valOne, valTwo - 1, -1))
     return list(range(valOne, valTwo + 1))
@@ -27,7 +21,7 @@ def listFromRange(valOne, valTwo) -> list:
 # PART ONE
 ##################
 
-boardOne = copy.deepcopy(board)
+board = {}
 numOverlap = 0
 
 for coordinate in coordinatesList:
@@ -35,10 +29,11 @@ for coordinate in coordinatesList:
     ys = listFromRange(coordinate[1], coordinate[3])
 
     if len(ys) != len(xs):
-        for y in ys:
-            for x in xs:
-                boardOne[y][x] += 1
-                if boardOne[y][x] == 2:
+        for x in xs:
+            for y in ys:
+                key = f'{x}-{y}' # fstrings are fastest way to do this
+                board[key] = board.get(key, 0) + 1
+                if board[key] == 2:
                     numOverlap += 1
 
 print('Part One Solution: %d' % numOverlap)
@@ -47,6 +42,7 @@ print('Part One Solution: %d' % numOverlap)
 # PART TWO
 ##################
 
+board = {}
 numOverlap = 0
 
 for coordinate in coordinatesList:
@@ -54,15 +50,17 @@ for coordinate in coordinatesList:
     ys = listFromRange(coordinate[1], coordinate[3])
 
     if len(ys) != len(xs):
-        for y in ys:
-            for x in xs:
-                board[y][x] += 1
-                if board[y][x] == 2:
+        for x in xs:
+            for y in ys:
+                key = f'{x}-{y}' # fstrings are fastest way to do this
+                board[key] = board.get(key, 0) + 1
+                if board[key] == 2:
                     numOverlap += 1
     else: # diagonal
-        for i in range(len(ys)):
-            board[ys[i]][xs[i]] += 1
-            if board[ys[i]][xs[i]] == 2:
+        for i in range(len(ys)): # could zip() but this is slightly faster :p
+            key = f'{xs[i]}-{ys[i]}' # fstrings are fastest way to do this
+            board[key] = board.get(key, 0) + 1
+            if board[key] == 2:
                 numOverlap += 1
 
 print('Part Two Solution: %d' % numOverlap)
