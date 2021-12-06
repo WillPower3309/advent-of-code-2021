7038c56fdef2ebf66dfbdfba78d491eb67398ae8
Will McKinnon <will.mckinnon@vidyard.com>
2021-12-06 14:02:49 -0500

    improved day 6

--- day6.py
+++ day6.py
@@ -1,3 +1,28 @@
+def calculateNumFish(initialNumList: list, numDays: int) -> int:
+    spawnTimer = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+    }
+    numFish = len(initialNumList)
+    for num in initialNumList:
+        spawnTimer[int(num)] += 1
+
+    for _ in range(numDays):
+        numReset = spawnTimer[0]
+        for i in range(8):
+            spawnTimer[i] = spawnTimer[i + 1]
+        spawnTimer[6] += numReset
+        spawnTimer[8] = numReset
+        numFish += numReset
+    return numFish
+
 f = open('input/day6.txt', 'r')
 initialState = f.readline()[:-1].split(',')
 
@@ -5,64 +30,11 @@ initialState = f.readline()[:-1].split(',')
 # PART ONE
 ##################
 
-spawnTimer = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-}
-numFish = 0
-
-numFish = len(initialState)
-for num in initialState:
-    spawnTimer[int(num)] += 1
-
-for _ in range(80):
-    numReset = spawnTimer[0]
-    for i in range(8):
-        spawnTimer[i] = spawnTimer[i + 1]
-    spawnTimer[6] += numReset
-    spawnTimer[8] = numReset
-    numFish += numReset
-
-print('Part One Solution: %d' % numFish)
+print('Part One Solution: %d' % calculateNumFish(initialState, 80))
 
 ##################
 # PART TWO
 ##################
 
-spawnTimer = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-}
-numFish = 0
-
-f = open('input/day6.txt', 'r')
-initialState = f.readline()[:-1].split(',')
-
-numFish = len(initialState)
-for num in initialState:
-    spawnTimer[int(num)] += 1
-
-for _ in range(256):
-    numReset = spawnTimer[0]
-    for i in range(8):
-        spawnTimer[i] = spawnTimer[i + 1]
-    spawnTimer[6] += numReset
-    spawnTimer[8] = numReset
-    numFish += numReset
-
-print('Part Two Solution: %d' % numFish)
+print('Part Two Solution: %d' % calculateNumFish(initialState, 256))
 
